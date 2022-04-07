860ba9140d1f3d8f0fee4943cb1c5a2ceea53ae1
Thiago de Souza <thiagobarrado99@gmail.com>
2022-04-07 11:11:29 -0300

    Adicionar arquivos de projeto.

---
+++ CronJobber/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace CronJobber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.time_remaining = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 25);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10800,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Intervalo em segundos";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(344, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "INICIAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Url a ser chamada";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "https://google.com";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Próxima chamada em";
+            // 
+            // time_remaining
+            // 
+            this.time_remaining.AutoSize = true;
+            this.time_remaining.Location = new System.Drawing.Point(296, 9);
+            this.time_remaining.Name = "time_remaining";
+            this.time_remaining.Size = new System.Drawing.Size(30, 13);
+            this.time_remaining.TabIndex = 6;
+            this.time_remaining.Text = "256s";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Forçar chamada";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 175);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.time_remaining);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CronJobber";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label time_remaining;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CronJobber/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CronJobber
+{
+    public partial class Form1 : Form
+    {
+        private bool running = false;
+
+        private System.Timers.Timer timer;
+        private System.Timers.Timer second_timer;
+
+        private int timer_delay;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new System.Timers.Timer();
+            timer.AutoReset = true;
+            timer.Elapsed += Timer_Elapsed;
+
+            second_timer = new System.Timers.Timer();
+            second_timer.AutoReset = true;
+            second_timer.Elapsed += Second_timer_Elapsed; ;
+        }
+
+        private void Second_timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            var x = Regex.Replace(time_remaining.Text, @"[^\d]", "");
+            int remaining = int.Parse( x );
+            remaining -= 1;
+            time_remaining.Invoke((MethodInvoker)delegate {
+                // Running on the UI thread
+                time_remaining.Text = remaining.ToString() + "s";
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            running = !running;
+
+            button1.Text = (running ? "PARAR" : "INICIAR");
+            if (running) { StartTimer(); } else { StopTimer(); };
+        }
+
+        private void StartTimer()
+        {
+            timer.Enabled = true;
+            timer.Interval = int.Parse(numericUpDown1.Value.ToString()) * 1000;
+            timer.Start();
+
+            second_timer.Enabled = true;
+            second_timer.Interval = 1000;
+            second_timer.Start();
+
+            time_remaining.Text = int.Parse(numericUpDown1.Value.ToString()) + "s";
+            timer_delay = int.Parse(numericUpDown1.Value.ToString());
+            Console.WriteLine("Ticking timer every: " + timer.Interval);
+
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("Timer ticked");
+            using (HttpClient client = new HttpClient()) 
+            {
+                _ = client.GetAsync(textBox1.Text).Result;
+            }
+            time_remaining.Invoke((MethodInvoker)delegate {
+                // Running on the UI thread
+                time_remaining.Text = timer_delay + "s";
+            });
+        }
+
+        private void StopTimer() 
+        {
+            timer.Enabled = false;
+            timer.Stop();
+
+            second_timer.Enabled = false;
+            second_timer.Stop();
+
+            time_remaining.Text = "0s";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            timer.Stop();
+
+            second_timer.Enabled = false;
+            second_timer.Stop();
+
+            time_remaining.Text = "0s";
+
+            Timer_Elapsed(null, null);
+
+            timer.Enabled = true;
+            timer.Interval = timer_delay * 1000;
+            timer.Start();
+
+            second_timer.Enabled = true;
+            second_timer.Interval = 1000;
+            second_timer.Start();
+
+            time_remaining.Text = timer_delay + "s";
+
+        }
+    }
+}
